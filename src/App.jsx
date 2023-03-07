c09067be69d45d18affe9d976a76f0340f2f3f1e
krish-98 <muralikrishnanm98@gmail.com>
2023-03-07 13:16:32 +0530

    responsive navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  StarsCanvas,
+} from "./components"
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  )
 }
 export default App
